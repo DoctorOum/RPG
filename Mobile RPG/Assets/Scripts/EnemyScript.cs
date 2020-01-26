2b329afb4c4882ccdb54128c99dc0f902d83ce0e
DoctorOum <43484827+DoctorOum@users.noreply.github.com>
2020-01-26 18:50:21 -0500

    added standard asset

---
+++ Mobile RPG/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    private int currentHelath;
+    public int maxHelath;
+
+    public float Power;
+    public float Attack;
+    public float Defense;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHelath = maxHelath;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
