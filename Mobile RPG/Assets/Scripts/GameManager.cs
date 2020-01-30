c1c78f4d43805186312b2e8575510e3fa5006690
DoctorOum <43484827+DoctorOum@users.noreply.github.com>
2020-01-30 15:51:08 -0500

    added wolf art to game

--- Mobile RPG/Assets/Scripts/GameManager.cs
+++ Mobile RPG/Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public Canvas battleSystem;
     public Canvas selectEnemy;
+
     // Start is called before the first frame update
     void Awake()
     {
--- Mobile RPG/Assets/Scripts/PlayerScript.cs
+++ Mobile RPG/Assets/Scripts/PlayerScript.cs
@@ -14,13 +14,14 @@ public class PlayerScript : MonoBehaviour
     public int maxHealth;
 
     public DamageManager damageManager;
-    //HealthManager Health;
+
     Enemy[] enemy;
+
     // Start is called before the first frame update
     void Start()
     {
         playerCurrentHealth = maxHealth;
-        //Health = GetComponent<HealthManager>();
+
         enemy = GameObject.FindObjectsOfType<Enemy>();
     }
 
@@ -31,6 +32,7 @@ public class PlayerScript : MonoBehaviour
 
         if (CrossPlatformInputManager.GetButtonDown("Attack"))
         {
+
             Debug.Log(enemy.Length);
             int damage = damageManager.Damage(Power, Attack, enemy[0].Defense);
             enemy[0].enemyCurrentHealth = damageManager.remainingHealth(enemy[0].enemyCurrentHealth, damage);
