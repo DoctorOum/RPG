e505a4de00102a9e488c81f31f89bfb417448594
DoctorOum <43484827+DoctorOum@users.noreply.github.com>
2020-01-30 18:11:20 -0500

    Enemies can be selected

--- Mobile RPG/Assets/Scripts/PlayerScript.cs
+++ Mobile RPG/Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,7 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerScript : MonoBehaviour
 {
+    bool choosingEnemy;
 
     public float Power = 2;
     public int Attack;
@@ -14,29 +15,84 @@ public class PlayerScript : MonoBehaviour
     public int maxHealth;
 
     public DamageManager damageManager;
+    public Enemy[] enemy = new Enemy[3];
 
-    Enemy[] enemy;
 
-    // Start is called before the first frame update
-    void Start()
+    public Canvas battleSystem;
+    public Canvas selectEnemy;
+
+    void Awake()
     {
-        playerCurrentHealth = maxHealth;
-
-        enemy = GameObject.FindObjectsOfType<Enemy>();
+        if (selectEnemy.enabled)
+        {
+            selectEnemy.enabled = false;
+        }
     }
 
-    // Update is called once per frame
+
+    void Start()
+    {
+        choosingEnemy = false;
+        playerCurrentHealth = maxHealth;
+        //enemy = GameObject.FindObjectsOfType<Enemy>();
+    }
+
+
     void Update()
     {
         if (playerCurrentHealth <= 0) Destroy(gameObject);
 
-        if (CrossPlatformInputManager.GetButtonDown("Attack"))
+        if (CrossPlatformInputManager.GetButtonDown("Attack")) //Attacking, selecting enemy now
         {
+            Debug.Log("There are " + enemy.Length + "Enemies!");
 
-            Debug.Log(enemy.Length);
-            int damage = damageManager.Damage(Power, Attack, enemy[0].Defense);
-            enemy[0].enemyCurrentHealth = damageManager.remainingHealth(enemy[0].enemyCurrentHealth, damage);
-            Debug.Log("Enemy takes damage");
+            battleSystem.enabled = false;
+            selectEnemy.enabled = true;
+            choosingEnemy = true;
+
+            Debug.Log("Choosing Enemy");
         }
+        if (choosingEnemy)
+        {
+            if (CrossPlatformInputManager.GetButtonDown("Enemy 1"))
+            {
+                Damaging(0);
+                selectEnemy.enabled = false;
+                battleSystem.enabled = true;
+                choosingEnemy = false;
+                Debug.Log("Enemy takes damage");
+                Debug.Log("Enemy's current health" + enemy[0].enemyCurrentHealth);
+            } else if (CrossPlatformInputManager.GetButtonDown("Enemy 2"))
+            {
+                Damaging(1);
+                selectEnemy.enabled = false;
+                battleSystem.enabled = true;
+                choosingEnemy = false;
+                Debug.Log("Enemy takes damage");
+                Debug.Log("Enemy's current health" + enemy[1].enemyCurrentHealth);
+            }else if (CrossPlatformInputManager.GetButtonDown("Enemy 3"))
+            {
+                Damaging(2);
+                selectEnemy.enabled = false;
+                battleSystem.enabled = true;
+                choosingEnemy = false;
+                Debug.Log("Enemy takes damage");
+                Debug.Log("Enemy's current health" + enemy[2].enemyCurrentHealth);
+            }else if (CrossPlatformInputManager.GetButtonDown("Go Back"))
+            {
+                selectEnemy.enabled = false;
+                battleSystem.enabled = true;
+                choosingEnemy = false;
+            }
+            else { 
+            Debug.Log("No longer Choosing enemy");
+            }
+        }
+    }
+
+    public void Damaging(int Enemy)
+    {
+        int damage = damageManager.Damage(Power, Attack, enemy[Enemy].Defense);
+        enemy[Enemy].enemyCurrentHealth = damageManager.remainingHealth(enemy[Enemy].enemyCurrentHealth, damage);
     }
 }
