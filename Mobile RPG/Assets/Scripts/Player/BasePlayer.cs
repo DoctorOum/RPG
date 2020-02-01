bc755d077bc2c779fdf85a29e3ff887f3e5f48fc
DoctorOum <43484827+DoctorOum@users.noreply.github.com>
2020-02-01 12:27:27 -0500

    Made some folders and player scripts

---
+++ Mobile RPG/Assets/Scripts/Player/BasePlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BasePlayer
+{
+    public Sprite Sprite;
+    public string name;
+    public float MaxHP;
+    public float CurrHP;
+    public float Attack;
+    public float Defense;
+    public float Accuracy;
+    public float CritChance;
+
+    //counter till they can use their Special
+    public float Special;
+
+    //counter till they can use their Ultimate
+    public float Ultimate;
+
+    public int TierLvl;
+
+
+
+    //Enemy types to be added at a later date
+    public enum Type
+    {
+        Water,
+        Fire,
+        Light,
+        Dark,
+        Earth,
+        Normal,
+        Wind
+
+    }
+
+    public Type EnemyType;
+    //
+}
---
+++ Mobile RPG/Assets/Scripts/Player/PlayerStateMachine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : MonoBehaviour
+{
+    public BasePlayer player;
+
+    public enum Turnstate
+    {
+        WAITING,
+        SELECTING,
+        ATTACK,
+        BLOCK,
+        SPECIAL,
+        ULTIMATE,
+        DEAD
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
