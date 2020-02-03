08b32b73d6abe93c151633bcb1e1564f59dd2f30
Michael DeProfio <Mikedeprofio@gmail.com>
2020-02-03 13:31:55 -0500

    getting rid of old code

--- Mobile RPG/Assets/Scripts/BattleManager.cs
+++ Mobile RPG/Assets/Scripts/BattleManager.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BattleManager : MonoBehaviour
 {
     //list of all players you command
     public List<GameObject> Players = new List<GameObject>();
 
-    //list of all enemie you will battle throughout scene
+    //list of all enemies you will battle throughout scene
     public List<GameObject> Wave1 = new List<GameObject>();
     public List<GameObject> Wave2 = new List<GameObject>();
     public List<GameObject> Wave3 = new List<GameObject>();
@@ -16,7 +17,7 @@ public class BattleManager : MonoBehaviour
     //master list that will hold both players and enemies to determine what order they attack in
     public List<GameObject> WholeCombat = new List<GameObject>();
 
-    //
+    //list for the current round, after someone does their action they are thrown out of the list
     public List<GameObject> CurrentRound = new List<GameObject>();
 
     // Start is called before the first frame update
